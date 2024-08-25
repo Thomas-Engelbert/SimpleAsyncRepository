ab42707eeca94f870f4894377c7d21225cc46c0d
Thomas Engelbert <thomas.engelbert@icloud.com>
2024-08-25 11:15:11 +0200

    Add first two tests

---
+++ SimpleAsyncRepository.InMemory.UnitTests/InMemoryBaseRepositoryTests.cs
@@ -0,0 +1,28 @@
+﻿namespace SimpleAsyncRepository.InMemory.UnitTests;
+
+using Shouldly;
+
+public class InMemoryBaseRepositoryTests
+{
+    [Fact]
+    public async Task ShouldlyBeEmptyAfterInstantiation ()
+    {
+        // Arrange + Act
+        InMemoryBaseRepository<TestModel> repository = new ();
+
+        // Assert
+        IList<TestModel> content = await repository.GetAll ();
+        content.ShouldBeEmpty ();
+    }
+
+    [Fact]
+    public async Task ShouldlyReturnCountOfZeroAfterInstantiation ()
+    {
+        // Arrange + Act
+        InMemoryBaseRepository<TestModel> repository = new ();
+
+        // Assert
+        int count = await repository.Count ();
+        count.ShouldBe ( 0 );
+    }
+}
