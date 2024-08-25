0677dfbb49a797a2433a6c68af59e24bef6e16d4
Thomas Engelbert <thomas.engelbert@icloud.com>
2024-08-25 11:15:01 +0200

    Add TestModel

---
+++ SimpleAsyncRepository.InMemory.UnitTests/TestModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleAsyncRepository.InMemory.UnitTests;
+
+using System;
+using SimpleAsyncRepository.Abstractions;
+
+/// <summary>
+/// Minimalistic model type for testing purposes
+/// </summary>
+public class TestModel : IModel
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+}
