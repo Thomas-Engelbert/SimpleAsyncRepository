9396a4f94f4fd7ed132be1997b397a4638b7c684
Thomas Engelbert <thomas.engelbert@icloud.com>
2024-08-25 12:04:56 +0200

    Add more tests

--- SimpleAsyncRepository.InMemory.UnitTests/InMemoryBaseRepositoryTests.cs
+++ SimpleAsyncRepository.InMemory.UnitTests/InMemoryBaseRepositoryTests.cs
@@ -4,6 +4,8 @@ using Shouldly;
 
 public class InMemoryBaseRepositoryTests
 {
+    #region Instantiation
+
     [Fact]
     public async Task ShouldlyBeEmptyAfterInstantiation ()
     {
@@ -25,6 +27,10 @@ public class InMemoryBaseRepositoryTests
         int count = await repository.Count ();
         count.ShouldBe ( 0 );
     }
+
+    #endregion
+
+    #region Upsert
 
     [Fact]
     public async Task ShouldAddMultipleEntitiesSimultaneously ()
@@ -69,4 +75,96 @@ public class InMemoryBaseRepositoryTests
         actualModelAfter.ShouldNotBeNull ();
         actualModelAfter.Name.ShouldBe ( nameAfter );
     }
+
+    [Fact]
+    public async Task ShouldCreateGuidsForInsertedItems ()
+    {
+        // Arrange
+        Guid existingGuid = Guid.NewGuid ();
+        TestModel model1 = new () { Id = existingGuid };
+        TestModel model2 = new ();
+        InMemoryBaseRepository<TestModel> repository = new ();
+
+        // Act
+        await repository.Upsert ( model1 );
+        await repository.Upsert ( model2 );
+        IList<TestModel> content = await repository.GetAll ();
+        IEnumerable<Guid> guids = content.Select ( x => x.Id );
+
+        // Assert
+        content.ShouldNotBeEmpty ();
+        guids.ShouldBeUnique ();
+        guids.ShouldNotContain ( Guid.Empty );
+    }
+
+    #endregion
+
+    #region Removal
+
+    [Fact]
+    public async Task ShouldRemoveExistingItem ()
+    {
+        // Arrange
+        Guid guid1 = Guid.NewGuid ();
+        Guid guid2 = Guid.NewGuid ();
+        TestModel model1 = new () { Id = guid1 };
+        TestModel model2 = new () { Id = guid2 };
+        InMemoryBaseRepository<TestModel> repository = new ();
+
+        // Act
+        await repository.Upsert ( model1 );
+        await repository.Upsert ( model2 );
+        await repository.RemoveById ( guid1 );
+        IList<TestModel> contents = await repository.GetAll ();
+        TestModel? actualModel1 = await repository.GetById ( guid1 );
+        TestModel? actualModel2 = await repository.GetById ( guid2 );
+
+        // Assert
+        contents.Count.ShouldBe ( 1 );
+        actualModel1.ShouldBeNull ();
+        actualModel2.ShouldNotBeNull ();
+        actualModel2.Id.ShouldBe ( guid2 );
+    }
+
+    [Fact]
+    public async Task ShouldNotThrowOnRemoveNonexistingItem ()
+    {
+        // Arrange
+        InMemoryBaseRepository<TestModel> repository = new ();
+
+        // Act
+        await repository.RemoveById ( Guid.NewGuid () );
+
+        // Assert
+        // If nothing was thrown, then this test has passed
+    }
+
+    [Fact]
+    public async Task ShouldNotTouchWrongItemsOnInvalidRemove ()
+    {
+        // Arrange
+        Guid guid1 = Guid.NewGuid ();
+        Guid guid2 = Guid.NewGuid ();
+        Guid guid3 = Guid.NewGuid ();
+        TestModel model1 = new () { Id = guid1 };
+        TestModel model2 = new () { Id = guid2 };
+        InMemoryBaseRepository<TestModel> repository = new ();
+
+        // Act
+        await repository.Upsert ( model1 );
+        await repository.Upsert ( model2 );
+        await repository.RemoveById ( guid3 );
+        IList<TestModel> contents = await repository.GetAll ();
+        TestModel? actualModel1 = await repository.GetById ( guid1 );
+        TestModel? actualModel2 = await repository.GetById ( guid2 );
+
+        // Assert
+        contents.Count.ShouldBe ( 2 );
+        actualModel1.ShouldNotBeNull ();
+        actualModel1.Id.ShouldBe ( guid1 );
+        actualModel2.ShouldNotBeNull ();
+        actualModel2.Id.ShouldBe ( guid2 );
+    }
+
+    #endregion
 }
