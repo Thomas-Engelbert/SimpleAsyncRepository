363d252dfd613150526885e3997828c1e9146a49
Thomas Engelbert <thomas.engelbert@icloud.com>
2024-08-25 11:06:00 +0200

    Add empty in memory implementation

---
+++ SimpleAsyncRepository.InMemory/InMemoryBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleAsyncRepository.InMemory;
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SimpleAsyncRepository.Abstractions;
+
+public class InMemoryBaseRepository<T> : IRepository<T> where T : class, IModel
+{
+    public Task<int> Count () => throw new NotImplementedException ();
+    public Task<IList<T>> GetAll () => throw new NotImplementedException ();
+    public Task<T?> GetById ( Guid id ) => throw new NotImplementedException ();
+    public Task RemoveById ( Guid id ) => throw new NotImplementedException ();
+    public Task Upsert ( T entity ) => throw new NotImplementedException ();
+}
