db924a27d7ab7c10abe640732fc65155b8231e57
Thomas Engelbert <thomas.engelbert@icloud.com>
2024-08-25 11:17:00 +0200

    Implement Count()

--- SimpleAsyncRepository.InMemory/InMemoryBaseRepository.cs
+++ SimpleAsyncRepository.InMemory/InMemoryBaseRepository.cs
@@ -1,13 +1,17 @@
 ﻿namespace SimpleAsyncRepository.InMemory;
 
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using SimpleAsyncRepository.Abstractions;
 
 public class InMemoryBaseRepository<T> : IRepository<T> where T : class, IModel
 {
-    public Task<int> Count () => throw new NotImplementedException ();
+    private readonly ConcurrentDictionary<Guid, T> dictionary = new ();
+
+    public Task<int> Count () => Task.FromResult ( this.dictionary.Count );
+
     public Task<IList<T>> GetAll () => throw new NotImplementedException ();
     public Task<T?> GetById ( Guid id ) => throw new NotImplementedException ();
     public Task RemoveById ( Guid id ) => throw new NotImplementedException ();
